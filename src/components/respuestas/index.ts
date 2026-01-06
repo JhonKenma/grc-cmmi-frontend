c759172c2ee00d32736428d34864a534b392d04e
Jhons Kenma <jhonskenma@gmail.com>
2026-01-06 16:54:12 -0500

    Actualiza índice de componentes de respuestas

--- src/components/respuestas/index.ts
+++ src/components/respuestas/index.ts
@@ -2,3 +2,8 @@
 
 export * from './PreguntaCard';
 export * from './ModalEvidencia';
+export * from './SeccionEvidencias';
+export * from './SelectorCumplimiento';
+export * from './SelectorNivelMadurez';
+export * from './VistaPreviewNivelMadurez';
+export * from './types';
