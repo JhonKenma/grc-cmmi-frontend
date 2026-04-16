8bb463221946e5abd511774b4fb39979b848b9ac
Jhons Kenma <jhonskenma@gmail.com>
2026-04-15 20:03:24 -0500

    Update 3 archivos

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,6 +6,8 @@
  */
 export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 
+export const AI_API_URL =
+  import.meta.env.VITE_AI_API_URL || 'http://localhost:8000/ai';
 /**
  * Información de la aplicación
  */
